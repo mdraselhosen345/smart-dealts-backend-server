c9c1d8698964405bea714cac1ae7155261252703
mdraselhosen345 <rashel099009@gmail.com>
2026-05-05 16:15:19 +0600

    added file sesh

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 // middleware 
 app.use(cors());
 app.use(express.json());
  
-const uri = "mongodb+srv://smartBdUser:3r0af7ctWOqE5wqP@cluster0.zbalbrq.mongodb.net/?appName=Cluster0";
+// const uri = "mongodb+srv://smartBdUser:3r0af7ctWOqE5wqP@cluster0.zbalbrq.mongodb.net/?appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zbalbrq.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient  
 const client = new MongoClient(uri, {
@@ -29,7 +33,7 @@ async function run () {
                
              const db = client.db("smart_db")
              const productsCollection = db.collection('products');
-             const bidsCollection = db.collection('dids')
+             const bidsCollection = db.collection('bids')
              const usersCollection = db.collection('users');
               
             //----------------Users API ------------
@@ -77,7 +81,7 @@ async function run () {
              app.get('/products/:id', async(req, res) => {
                 const id = req.params.id;
                 const query = { _id: new ObjectId(id)}
-                const result =await productsCollection.find(query);
+                const result =await productsCollection.findOne(query);
                 res.send(result);
              })
 
@@ -97,7 +101,7 @@ async function run () {
                         price: updatedProduct.price
                     }
                 }
-                const result =await productsCollection.updateOne(query.update)
+                const result =await productsCollection.updateOne(query, update)
                 res.send(result)
              })
 
@@ -116,12 +120,12 @@ async function run () {
                     query.buyer_email = email;
                 }
 
-                const cursor = bidsCollection.find(query);
+                // const cursor = bidsCollection.find(query);
                 const result = await cursor.toArray();
                 res.send(result); 
             })
             
-                app.get('products/bids/:productId', async(req, res) => {
+                app.get('/products/bids/:productId', async(req, res) => {
                     const productId = req.params.productId;
                     const query = { product: productId}
                     const cursor = bidsCollection.find(query).sort({bid_price: -1})
@@ -130,22 +134,28 @@ async function run () {
                 })
 
                 app.get('/bids', async(req, res) => {
-                   
+                    const email = req.query.email;
                     const query = {};
-                    if(query.email) {
-                        query.email = email;
+                    if(email) {
+                        query.buyer_email = email;
                     }
 
                     const cursor = bidsCollection.find(query);
-                    const result = await cursor.toArray();
-                    res.send(result);
-                })
+                 const result = await cursor.toArray();
+                     res.send(result);
+                 })
 
              app.post('/bids', async(req, res) => {
                 const newBid = req.body;
                 const result = await bidsCollection.insertOne(newBid);
                 res.send(result);
              })
+             app.delete('/bids/:id', async(req, res) => {
+                const id = req.params.id;
+                const query = { _id: new ObjectId (id)}
+                const result = await bidsCollection.deleteOne(query)
+                res.send(result);
+             } )
 
              await client.db("admin").command({ ping: 1});
              console.log("Pinged your deployment. You successfully connected to MongoDB");
